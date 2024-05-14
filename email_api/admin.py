61137d7fd42ae7e60376516fb75934dd3bad97a9
Julius Boakye <66005635+Darkbeast-glitch@users.noreply.github.com>
2024-05-14 22:18:47 +0000

    chore: Update ordering in EmailAdmin to sort emails by ID in ascending order

--- email_api/admin.py
+++ email_api/admin.py
@@ -9,7 +9,7 @@ class EmailAdmin(admin.ModelAdmin):
     list_display = ('id', 'email')
     search_fields = ['email',]  # fields to search
     list_filter = ('id', 'email')  # fields to filter
-    ordering = ['-id']  # order by sent_at in descending order
+    ordering = ['id']  # order by sent_at in descending order
 
 
 admin.site.register(Email, EmailAdmin)
