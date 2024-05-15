06accc53ec0715799616d3f95838011c812dc828
Julius Boakye <66005635+Darkbeast-glitch@users.noreply.github.com>
2024-05-15 23:31:02 +0000

    chore: Update email API URLs to remove trailing slashes

--- email_api/urls.py
+++ email_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import EmailCreateView, EmailListView
 
 urlpatterns = [
-    path('email/', EmailCreateView.as_view(), name='create_email'),
-    path('list-emails/', EmailListView.as_view(), name='list_emails'),
+    path('email', EmailCreateView.as_view(), name='create_email'),
+    path('list-emails', EmailListView.as_view(), name='list_emails'),
 ]
